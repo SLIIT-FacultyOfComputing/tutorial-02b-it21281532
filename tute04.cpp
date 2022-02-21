a3c1f7b9b6d643b32cc09936fa218b3138844cd5
it21281532 <99232552+it21281532@users.noreply.github.com>
2022-02-21 22:27:05 +0000

    tutorial 2 submitted

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,16 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  if(no==1)
+    {
+      return 1;
+    }
+    return no*Factorial(no-1);
+
+}
+long nCr(int n, int r)
+{
+  return Factorial(n)/(Factorial(r)*Factorial(n-r));
+}
